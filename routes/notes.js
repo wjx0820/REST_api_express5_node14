d6f5071212e5cd969af590f5b773cc033339cef9
Jexxie <1989wjx@gmail.com>
2020-05-18 12:00:00 +0800

    validate input

--- routes/notes.js
+++ routes/notes.js
@@ -1,11 +1,18 @@
 export function list(req, res) {
-	const { sort } = req.query
+	let { sort = 'desc' } = req.query
+	const validSorts = new Set(['desc', 'asc'])
+	if (!validSorts.has(sort.toLowerCase())) {
+		return res.status(400).send('Invalid sort params')
+	}
 	console.log({ sort })
 	res.json([])
 }
 
 export function create(req, res) {
 	const { title, body } = req.body
+	if (title === undefined || body === undefined) {
+		return res.status(400).send('Missing title or body')
+	}
 	console.log(`${title} and ${body} received!`)
 	res.send('ok')
 }
@@ -18,6 +25,9 @@ export function read(req, res) {
 export function update(req, res) {
 	const { id } = req.params
 	const { title, body } = req.body
+	if (title === undefined && body === undefined) {
+		return res.status(400).send('Missing title and body')
+	}
 	console.log(`updating ${id} with ${title} and ${body}`)
 	res.send('update')
 }
