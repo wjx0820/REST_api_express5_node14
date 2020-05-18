341951e71b5e5adcd564ee537ca6808e4ec39e76
Jexxie <1989wjx@gmail.com>
2020-05-18 12:47:14 +0800

    create a model to manage data

--- routes/notes.js
+++ routes/notes.js
@@ -1,39 +1,45 @@
+import * as Note from '../model/note.js'
+
 export function list(req, res) {
 	let { sort = 'desc' } = req.query
 	const validSorts = new Set(['desc', 'asc'])
 	if (!validSorts.has(sort.toLowerCase())) {
 		return res.status(400).send('Invalid sort params')
 	}
-	console.log({ sort })
-	res.json([])
+	const notes = Note.getNotes(sort)
+	res.json({ notes })
 }
 
-export function create(req, res) {
+export async function create(req, res) {
 	const { title, body } = req.body
 	if (title === undefined || body === undefined) {
 		return res.status(400).send('Missing title or body')
 	}
-	console.log(`${title} and ${body} received!`)
+	const note = await Note.createNote({ title, body })
+	console.log({ note })
 	res.send('ok')
 }
 
 export function read(req, res) {
 	const { id } = req.params
-	res.json({ id, title: 'a simple title', body: 'this is my note' })
+	const note = Note.getNote(id)
+	res.json({ note })
 }
 
-export function update(req, res) {
+export async function update(req, res) {
 	const { id } = req.params
 	const { title, body } = req.body
 	if (title === undefined && body === undefined) {
 		return res.status(400).send('Missing title and body')
 	}
-	console.log(`updating ${id} with ${title} and ${body}`)
+	const note = await Note.updateNote(id, { title, body })
+	console.log({ note })
 	res.send('update')
 }
 
 export function deleteNote(req, res) {
 	const { id } = req.params
-	console.log(`del ${id}`)
+	const success = Note.deleteNote(id)
+	console.log(`deleting ${id}`, success)
 	res.send('delete')
 }
